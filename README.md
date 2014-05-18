32bd0fbae857e13fcd94e4e651374094de91d711
lighter <apple.xva@gmail.com>
2014-05-18 14:44:11 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#Qunit Test Sample
+QUnit 練習範本
+ref: http://qunitjs.com/cookbook/
---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>QUnit sample</title>
+  <script src="qunit.js"></script>
+  <link rel="stylesheet" href="qunit.css">
+</head>
+<body>
+  <div id="qunit"></div>
+  <div id="qunit-fixture"></div>
+</body>
+</html>
